d02e40f00f7f1a7004ef8e0bc7b7775754b72ffa
quikkkk <kexitttttt@gmail.com>
2025-09-07 19:25:08 +0300

    enhance Kafka producer with transactional support

--- src/main/java/kafka/Producer.java
+++ src/main/java/kafka/Producer.java
@@ -13,8 +13,11 @@ import java.util.concurrent.ExecutionException;
 
 import static org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION;
 import static org.apache.kafka.clients.producer.ProducerConfig.RETRIES_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.TRANSACTIONAL_ID_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 public class Producer {
@@ -34,11 +37,27 @@ public class Producer {
         properties.put(VALUE_SERIALIZER_CLASS_CONFIG, OrderSerializer.class.getName());
         properties.put(ACKS_CONFIG, "all");
         properties.put(RETRIES_CONFIG, 3);
+        properties.put(MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
+        properties.put(ENABLE_IDEMPOTENCE_CONFIG, true);
+        properties.put(TRANSACTIONAL_ID_CONFIG, "order-transactional-producer");
 
         try (var producer = new KafkaProducer<String, Order>(properties)) {
-            ProducerRecord<String, Order> record = new ProducerRecord<>("sandbox", testOrder.getId(), testOrder);
-            var metadata = producer.send(record).get();
-            System.out.println("Повідомлення відправлено успішно: " + metadata.topic());
+            producer.initTransactions();
+
+            try {
+                producer.beginTransaction();
+                ProducerRecord<String, Order> record = new ProducerRecord<>("orders", testOrder.getId(), testOrder);
+
+                var metadata = producer.send(record).get();
+                System.out.println("Повідомлення відправлено успішно: " + metadata.topic());
+
+                producer.commitTransaction();
+                System.out.println("Транзакція успішно завершена");
+            } catch (Exception e) {
+                producer.abortTransaction();
+                System.err.println("Транзакція відкатана через помилку: " + e.getMessage());
+                throw e;
+            }
         } catch (ExecutionException | InterruptedException e) {
             System.err.println("Помилка при відправці повідомлення: " + e.getMessage());
             Thread.currentThread().interrupt();
